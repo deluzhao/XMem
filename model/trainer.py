5ba990a40f25274f27ca2c158fdf69fd5db650e2
deluzhao <deluzhaodev@gmail.com>
2023-07-21 15:23:26 -0700

    resize

--- model/trainer.py
+++ model/trainer.py
@@ -84,6 +84,12 @@ class XMemTrainer:
         selector = data['selector'].unsqueeze(2).unsqueeze(2)
 
         resize_frames = transforms.Compose([transforms.Resize((480,480))])
+
+        B, C, T, W, H = frames.size() 
+        frames_480p = frames.reshape(B*C, T, W, H).clone()
+        frames_480p = F.interpolate(frames_480p, size=(480, 480), mode='bilinear', align_corners=False)
+        frames_480p = frames_480p.reshape(B, C, T, 480, 480)
+
         frames_480p = resize_frames(frames)
         print(frames.shape)
         print(frames_480p.shape)
